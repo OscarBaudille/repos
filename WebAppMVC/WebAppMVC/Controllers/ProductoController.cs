3925ed3fd7209e344c9af7b3895637ef6e93ce0f
OscarBaudille <oscarbaudille@hotmail.com>
2025-06-11 17:03:43 -0300

    cambio en index.cshtml de Producto

---
+++ WebAppMVC/WebAppMVC/Controllers/ProductoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+
+    public class ProductoController : Controller
+    {
+        public ActionResult Index()
+        {
+            var productos = new List<Producto>
+        {
+            new Producto { Id = 1, Nombre = "Laptop", Precio = 1500 },
+            new Producto { Id = 2, Nombre = "Mouse", Precio = 25 },
+            new Producto { Id = 3, Nombre = "Teclado", Precio = 47 }
+        };
+            return View(productos);
+        }
+    }
+
+
+}
